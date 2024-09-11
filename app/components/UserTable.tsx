cb5e612ee57c421b18dcf1e4203fed11aa6876f9
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-09-11 16:14:03 +0200

    tablecomponents

--- app/components/UserTable.tsx
+++ app/components/UserTable.tsx
@@ -1,36 +1,20 @@
 import React from 'react';
 import { User } from '../api/Type';
-
+import Table from './Table';
 
 interface UserTableProps {
   users: User[];
 }
 
 const UserTable: React.FC<UserTableProps> = ({ users }) => {
-  return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full bg-white shadow-md">
-        <thead>
-          <tr className="border-b bg-gray-200">
-            <th className="p-4 text-left ">Username</th>
-            <th className="p-4 text-left ">Points Envoyés</th>
-            <th className="p-4 text-left ">Points Reçus</th>
-            <th className="p-4 text-left ">Solde</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.discordUsername} className=" hover:bg-gray-50">
-              <td className="p-4">{user.discordUsername}</td>
-              <td className="p-4">{user.pointsSent}</td>
-              <td className="p-4">{user.pointsReceived}</td>
-              <td className="p-4">{user.balance}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  const headers = [
+    { key: 'discordUsername' as keyof User, label: 'Username' },
+    { key: 'pointsSent' as keyof User, label: 'Points Envoyés' },
+    { key: 'pointsReceived' as keyof User, label: 'Points Reçus' },
+    { key: 'balance' as keyof User, label: 'Solde' },
+  ];
+
+  return <Table data={users} headers={headers} />;
 };
 
 export default UserTable;
